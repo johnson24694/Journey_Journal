52ab01f45152c3cb2fbee724539d94eda8a2d57c
BCWagnon <bridget.wagnon4552@gmail.com>
2023-07-20 15:03:37 -0500

    First commit Client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import JournalForm from './components/JournalForm';
+function App() {
+  return (
+    <div className="App">
+      <JournalForm/>
+    </div>
+  );
+}
+export default App;
