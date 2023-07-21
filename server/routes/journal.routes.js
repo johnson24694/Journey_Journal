f92fb0429f2630e99b6b45e3bdc25e9b922f437b
BCWagnon <bridget.wagnon4552@gmail.com>
2023-07-21 16:46:59 -0500

    Backend for Journal entrys - note had to remove validations will add back later

--- server/routes/journal.routes.js
+++ server/routes/journal.routes.js
@@ -2,5 +2,6 @@ const JournalController = require('../controller/journal.controller');
 
 module.exports = (app) => {
     app.get('/api', JournalController.index);
+    app.post('/api/journal', JournalController.createJournal);
 }
 
