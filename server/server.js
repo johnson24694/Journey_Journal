98389b1c7f48928692ca22ae1676f2b71f802500
BCWagnon <bridget.wagnon4552@gmail.com>
2023-07-23 00:26:00 -0500

    Created journalForm and made it functional with backend

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 app.use(cors({credentials:true, origin:'http://localhost:3000'}));                 
-require('./routes/journal.routes')(app);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 require('./config/mongoose.config');
 require('./config/jwt.config');
 const UserRoutes = require('./routes/user.routes')(app);
-const Routes = require('./routes/journal.routes')(app);
+const JournalRoutes = require('./routes/journal.routes')(app);
 
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
