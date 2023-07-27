3f1b5d7ea7bd8550a4fbaa28752a71318409540a
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-07-26 21:01:39 -0500

    updates

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');    
 const cookieParser = require('cookie-parser');
-app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
-require('./routes/journal.routes')(app);
-app.use(cookieParser());
-
 require('./config/mongoose.config');
+require('dotenv').config();
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cookieParser());
+require('./routes/journal.routes')(app);
 require('./config/jwt.config');
+
 const UserRoutes = require('./routes/user.routes')(app);
 const JournalRoutes = require('./routes/journal.routes')(app);
 
